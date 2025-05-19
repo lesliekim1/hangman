9b4501a8f32cb6c7bb76591200645faf895cfec1
lesliekim1 <lskim@loyola.edu>
2025-05-19 18:05:07 -0400

    add game over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+from scipy.stats import false_discovery_control
+
 word_list = ["aardvack", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
@@ -10,14 +12,28 @@ word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 
-guess = input("Guess a letter: ").lower()
+game_over = False
+correct_letters = []
 
-display = ""
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+
+        elif letter in correct_letters:
+            display += letter
+
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        game_over = True
 
 
